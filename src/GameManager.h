e2685c3c10a4b75a0fb965f1dc8d213780414055
burra <gezerbugra@gmail.com>
2024-01-27 16:49:18 +0300

    game manager updated

--- src/GameManager.h
+++ src/GameManager.h
@@ -1,9 +1,11 @@
 #include "game_elements/Ball.h"
 #include "game_elements/Bat.h"
+#include "game_elements/GameObject.h"
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 #include <iostream>
+#include <vector>
 
 class GameManager {
 
@@ -14,12 +16,12 @@ public:
 
     window.create(sf::VideoMode(), "Ping Pong Game", sf::Style::Fullscreen);
     window.setVerticalSyncEnabled(true);
+    window.clear(sf::Color::Black);
 
     return this;
   }
 
   void run() {
-
     while (window.isOpen()) {
       sf::Event event;
 
@@ -29,19 +31,8 @@ public:
         }
       }
 
-      window.clear(sf::Color::Black);
-
-      Bat bat(50.f, 250.f, window.getSize());
-      bat.shape->setFillColor(sf::Color::Yellow);
-      bat.centerHorizontally();
-
-      Ball ball(100.f, window.getSize());
-
-      ball.shape->setFillColor(sf::Color::Red);
-      ball.shape->move(500.f, 250.f);
-      window.draw(*ball.shape);
-      window.draw(*bat.shape);
-
+      createGameObjects();
+      drawGameObjects();
       window.display();
     }
   }
@@ -49,4 +40,23 @@ public:
 private:
   sf::Font arialFont;
   sf::RenderWindow window;
+  std::vector<GameObject> gameObjects;
+
+  void createGameObjects() {
+    Bat bat(50.f, 250.f, window.getSize());
+    bat.shape->setFillColor(sf::Color::Yellow);
+    bat.centerHorizontally();
+
+    Ball ball(100.f, window.getSize());
+    ball.shape->setFillColor(sf::Color::Red);
+    ball.shape->move(500.f, 250.f);
+
+    gameObjects.insert(gameObjects.end(), {ball, bat});
+  }
+
+  void drawGameObjects() {
+    for (GameObject gameObject : gameObjects) {
+      window.draw(*(gameObject.shape));
+    }
+  }
 };
